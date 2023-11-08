0479857783369b88296e242d02bd136e07bdafbc
TatianaYakushonok <yakush2@yandex.ru>
2023-11-08 22:37:56 +0700

    feat: add script.js (Wed, 08 Nov 2023 22:37:56 +0700)

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const nameGoods = 'Телевизор DEXP';
+const amount = 15;
+const category = 'Техника для дома';
+const price = 1000;
+const totalPrice = amount * price;
+
+console.log(nameGoods);
+console.log(totalPrice);
