b2eafb951bc3046f0d8b5ad692719a980fad1e89
TatianaYakushonok <yakush2@yandex.ru>
2023-12-12 13:20:50 +0700

    feat: add constant for element modal (Tue, 12 Dec 2023 13:20:49 +0700)

--- js/script.js
+++ js/script.js
@@ -1,35 +1,8 @@
-// Модуль 4 урок 1
-{
-  const nameGoods = 'Телевизор DEXP';
-  const amount = 15;
-  const category = 'Техника для дома';
-  const price = 1000;
-  const totalPrice = amount * price;
+'use strict';
 
-  console.group('Модуль 4 урок 1');
-  console.log('Товар: ' + nameGoods);
-  console.log('Общая стоимость: ' + totalPrice);
-  console.groupEnd();
-}
-
-// Модуль 4 урок 2
-{
-  /*const nameGoods = prompt('Введите наименование товара');
-  const category = prompt('Введите категорию товара');
-  const amount = parseInt(prompt('Введите количество товара'));
-  const price = parseInt(prompt('Введите цену товара'));
-
-  if (!isNaN(amount) && !isNaN(price)) {
-    const totalPrice = amount * price;
-
-    console.group('Модуль 4 урок 2');
-    console.log(typeof amount);
-    console.log(typeof price);
-    console.log(`На складе ${amount} единицы товара "${nameGoods}" на сумму ${totalPrice.toLocaleString()} ₽`);
-    console.groupEnd();
-  } else {
-    console.group('Модуль 4 урок 2');
-    console.log('Вы ввели некорректные данные! Введите число');
-    console.groupEnd();
-  }*/
-}
+const modalTitle = document.querySelector('.modal__title');
+const modalId = document.querySelector('.modal__id');
+const form = document.querySelector('.form');
+const formCheckDiscount = document.querySelector('.form__checkbox_discount');
+const formInputDiscount = document.querySelector('.form__input_discount');
+const formTotalPrice = document.querySelector('.form__total-price');
