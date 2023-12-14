b739ede35c2da72b125dc2a2a8095df648bbbfd1
TatianaYakushonok <yakush2@yandex.ru>
2023-12-14 14:00:16 +0700

    feat: add tack lesson 3 (Thu, 14 Dec 2023 14:00:15 +0700)

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ const form = document.querySelector('.form');
 const formCheckDiscount = document.querySelector('.form__checkbox_discount');
 const formInputDiscount = document.querySelector('.form__input_discount');
 const formTotalPrice = document.querySelector('.form__total-price');
+
+import goods from './goods.json' assert { type: 'json' };
+
+const createRow = (obj) => {
+  const tr = document.createElement('tr');
+  for (let key in obj) {
+    tr.innerHTML += `<td>${obj[key]}</td>`;
+  }
+
+  return tr;
+};
+
+const renderGoods = (arrObj) => {
+  const table = document.createElement('table');
+  arrObj.map((item) => {
+    table.append(createRow(item));
+  });
+
+  return table;
+};
+
+console.log(renderGoods(goods));
