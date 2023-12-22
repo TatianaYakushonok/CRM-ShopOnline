506507c641f99b4e40a3c08997ce024bb7996f90
TatianaYakushonok <yakush2@yandex.ru>
2023-12-22 21:52:29 +0700

    feat: add calculate total sum (Fri, 22 Dec 2023 21:52:28 +0700)

--- js/script.js
+++ js/script.js
@@ -12,23 +12,10 @@ const btnClose = document.querySelector('.form__btn_close');
 
 import goods from './goods.json' assert { type: 'json' };
 
-const openModal = (e) => {
-  if (e.target.closest('.filter__btn-goods')) {
-    modal.classList.add('modal__open');
-    window.addEventListener('keydown', closeModal);
-  }
-};
-
-const closeModal = (e) => {
-  if (e.target.closest('.form__btn_close') || e.target === modal) {
-    modal.classList.remove('modal__open');
-    window.removeEventListener('keydown', closeModal);
-  }
-};
-
 const createRows = ({ id, title, category, units, count, price }) => {
   const tr = document.createElement('tr');
   tr.classList.add('table__row');
+  tr.setAttribute('data-id', id);
   tr.insertAdjacentHTML(
     'beforeend',
     `
@@ -81,30 +68,146 @@ const createRows = ({ id, title, category, units, count, price }) => {
 
 const renderGoods = (arrObj) => {
   const table = document.querySelector('.table__body');
+  table.innerHTML = '';
   const rows = arrObj.map(createRows);
   table.append(...rows);
 
   return table;
 };
 
-const removeRow = (table) => {
+const removeGoodData = (id) => {
+  const index = goods.findIndex((item) => item.id.toString() === id);
+  if (index !== -1) {
+    goods.splice(index, 1);
+  }
+};
+
+const removeRow = (totalPrice, table, totalSum, formTotalSum) => {
   table.addEventListener('click', (e) => {
     const target = e.target;
     if (target.closest('.table__btn_del')) {
+      const id = target.closest('.table__row').dataset.id;
       target.closest('.table__row').remove();
-      console.log(table);
+      removeGoodData(id);
+      totalPrice = calculateTotalSum(goods);
+      addTotalSum(totalPrice, totalSum, formTotalSum);
     }
   });
 };
 
-const init = () => {
-  const table = renderGoods(goods);
-  console.log(table);
+const modalControl = (btnGoods, modal) => {
+  const openModal = () => {
+    modal.classList.add('modal__open');
+  };
 
-  removeRow(table);
+  const closeModal = () => {
+    modal.classList.remove('modal__open');
+  };
 
   btnGoods.addEventListener('click', openModal);
-  modal.addEventListener('click', closeModal);
+
+  modal.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.closest('.form__btn_close') || target === modal) {
+      closeModal();
+    }
+  });
+
+  return {
+    closeModal,
+  };
+};
+
+const addGoodData = (totalPrice, newGood, totalSum, formTotalSum) => {
+  goods.push(newGood);
+  addTotalSum(totalPrice, totalSum, formTotalSum);
+};
+
+const addGoodPage = (newGood, table) => {
+  const newRow = createRows(newGood);
+  table.append(newRow);
+};
+
+const formControl = (
+  totalPrice,
+  form,
+  table,
+  closeModal,
+  totalSum,
+  formTotalSum,
+) => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const newGoods = Object.fromEntries(formData);
+    /*const id = goods.length;
+    newGoods[id] = id;
+    console.log(newGoods[id]);*/
+    addGoodData(totalPrice, newGoods, totalSum, formTotalSum);
+    addGoodPage(newGoods, table);
+    totalPrice = calculateTotalSum(goods);
+    addTotalSum(totalPrice, totalSum, formTotalSum);
+
+    form.reset();
+    closeModal();
+  });
+  addTotalSum(totalPrice, totalSum, formTotalSum);
+};
+
+const calculateFormTotalSum = (totalPrice, form, totalSum, formTotalSum) => {
+  const formTotalPrice = form.elements.price;
+  const formTotalCount = form.elements.count;
+
+  formTotalPrice.addEventListener('blur', () => {
+    totalPrice += +formTotalPrice.value * +formTotalCount.value;
+    addTotalSum(totalPrice, totalSum, formTotalSum);
+  });
+};
+
+const discountControl = (form) => {
+  const checkDiscount = form.elements.discount[0];
+  const discount = form.elements.discount[1];
+
+  form.addEventListener('change', () => {
+    if (checkDiscount.checked) {
+      discount.disabled = false;
+    } else {
+      discount.disabled = true;
+      discount.value = '';
+    }
+  });
+};
+
+const calculateTotalSum = (obj) => {
+  const totalPrice = obj.reduce(
+    (acc, item) => (acc += item.price * item.count),
+    0,
+  );
+
+  return totalPrice;
+};
+
+const addTotalSum = (totalPrice, totalSum, formTotalSum) => {
+  totalSum.textContent = `$${totalPrice.toLocaleString('en', {
+    minimumFractionDigits: 2,
+  })}`;
+  formTotalSum.textContent = totalSum.textContent;
+};
+
+const init = () => {
+  const totalSum = document.querySelector('.header__total-sum');
+  const formTotalSum = document.querySelector('.form__total-sum');
+
+  const table = renderGoods(goods);
+  const totalPrice = calculateTotalSum(goods);
+
+  removeRow(totalPrice, table, totalSum, formTotalSum);
+
+  const { closeModal } = modalControl(btnGoods, modal);
+  formControl(totalPrice, form, table, closeModal, totalSum, formTotalSum);
+  discountControl(form);
+  addTotalSum(totalPrice, totalSum, formTotalSum);
+  calculateFormTotalSum(totalPrice, form, totalSum, formTotalSum);
 };
 
 init();
